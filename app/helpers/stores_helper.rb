9838580014a1b675068ddc64e9dd1172b8084d43
Jeongbin Kim <dev@menumap.co.kr>
2016-01-15 06:54:44 +0000

    Complete pension and speeto stores#show pages

--- app/helpers/stores_helper.rb
+++ app/helpers/stores_helper.rb
@@ -1,4 +1,6 @@
 module StoresHelper
+	include ActionView::Helpers::NumberHelper
+
 	def news_pub_date(date)
 		DateTime.parse(date).strftime("%Y-%m-%d %H:%M")
 	end
@@ -19,6 +21,23 @@ module StoresHelper
 		else
 			"yellow"
 		end
+	end
+
+	def pension_multiplier(num, multiplier)
+		number_with_delimiter(num * multiplier)
+	end
+
+	def special_number(rank)
+		n = rank.rank_nums.find_by_special(true).num
+		if n.nil?
+			'각조'
+		else
+			n.to_s + '조'
+		end
+	end
+
+	def number_or_blank(rank_num)
+		rank_num.num.nil? ? '-' : rank_num.num
 	end
 end
 
